3538c9422eafeda3ef28d5c9ef09454d420ac99f
David Salgado <davis6620@hotmail.com>
2019-08-20 08:59:10 -0500

    Fin sección 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
+import { reject } from 'q';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,16 +10,47 @@ import { Producto } from '../interfaces/producto.interface';
 export class ProductosService {
  cargando = true;
  productos: Producto[] = [];
+ productosFiltrado: Producto[] = [];
   constructor( private http: HttpClient) {
     this.cargarProductos();
   }
 
   private cargarProductos() {
-    this.http.get('https://resistencias-79d56.firebaseio.com/productos_idx.json')
-    .subscribe(  (resp: Producto[]) => {
-        this.productos = resp;
-        // console.log(resp);
-        this.cargando = false;
+
+    // tslint:disable-next-line:no-unused-expression
+    return new Promise (( resolve, reject ) => {
+      this.http.get('https://resistencias-79d56.firebaseio.com/productos_idx.json')
+      .subscribe(  (resp: Producto[]) => {
+          this.productos = resp;
+          this.cargando = false;
+          resolve();
+      });
+    });
+  }
+
+
+  getProducto(id: string) {
+    return this.http.get(`https://resistencias-79d56.firebaseio.com/productos/${ id }.json`);
+  }
+
+  buscarProducto(termino: string) {
+    if ( this.productos.length === 0) {
+      this.cargarProductos().then (() => {
+        this.filtrarProductos( termino );
+      });
+    } else{
+      this.filtrarProductos( termino );
+    }
+  }
+
+  filtrarProductos(termino: string) {
+    this.productosFiltrado = [];
+    termino = termino.toLocaleLowerCase();
+    this.productos.forEach( prod => {
+      const titulotoLower = prod.titulo.toLocaleLowerCase();
+      if (prod.categoria.indexOf(termino) >= 0 || titulotoLower.indexOf(termino) >= 0) {
+        this.productosFiltrado.push(prod);
+      }
     });
   }
 }
